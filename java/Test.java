32b4527f5bdc98904d709db8bab2a3c38b973557
baihuanyu <522858867@qq.com>
2019-07-11 23:28:53 +0800

    java

---
+++ java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int age = 18 ;
+        System.out.println(age);
+        String name = "白桓宇";
+        System.out.println((name));
+    }
+}
